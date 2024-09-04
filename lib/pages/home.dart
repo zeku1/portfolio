20ce6f72f5d72e5a750d1076023f15d6452b5c90
Ezekiel Kim Lucena <zekielucena@gmail.com>
2024-09-04 18:10:57 +0800

    added goggle fonts

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,20 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
-
-class HomePage extends StatelessWidget{
-    const HomePage({super.key});
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: appBar(),
       backgroundColor: Colors.white,
-      body: Column(children: [
-      ]),
+      body: Column(children: []),
     );
-}
+  }
 
-  AppBar appBar(){
-    return AppBar(title: Text('hello world!'));
+  AppBar appBar() {
+    return AppBar(title: Text('hello world!', style: GoogleFonts.poppins()));
   }
 }
