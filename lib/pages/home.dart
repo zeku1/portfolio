5d1ace2cd4d5f5bd8ba17dd0304f18e5d9d99591
Ezekiel Kim Lucena <zekielucena@gmail.com>
2024-09-05 08:42:32 +0800

    added appbar

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
@@ -9,11 +9,56 @@ class HomePage extends StatelessWidget {
     return Scaffold(
       appBar: appBar(),
       backgroundColor: Colors.white,
-      body: Column(children: []),
+      body: Column(),
     );
   }
 
   AppBar appBar() {
-    return AppBar(title: Text('hello world!', style: GoogleFonts.poppins()));
+    return AppBar(
+      toolbarHeight: 150.0,
+      titleSpacing:
+          0, // Removes default spacing for title if you want full control
+      flexibleSpace: Padding(
+        padding: const EdgeInsets.symmetric(
+            horizontal: 20.0, vertical: 30.0), // Adjust padding as needed
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Container(
+              margin: const EdgeInsets.all(15),
+              alignment: Alignment.center,
+              child: SvgPicture.asset(
+                'assets/icons/logo.svg',
+                width: 60, // Adjust the width as needed
+                height: 60, // Adjust the height as needed
+              ),
+            ),
+            GestureDetector(
+              onTap: () {},
+              child: Container(
+                height: 45,
+                width: 87,
+                child: const Center(
+                  child: Text(
+                    'Menu',
+                    style: TextStyle(
+                        color: Colors.white,
+                        fontWeight: FontWeight.w400,
+                        fontSize: 21),
+                  ),
+                ),
+                decoration: BoxDecoration(
+                  gradient: const LinearGradient(
+                      colors: [Color(0xff7981FC), Color(0xff8BE5DC)]),
+                  borderRadius: BorderRadius.circular(50),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+      backgroundColor: Colors.white,
+      elevation: 0.0,
+    );
   }
 }
